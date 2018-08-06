9e82fbfbbce2f258e97cfe5ca53e5e8be7a04e31
marcvanmelle <mvanmelle@everquote.com>
2018-08-06 09:23:56 -0400

    FACETOME-16 Alignment Weighting

--- internal/npcgenerator/get_alignment.go
+++ internal/npcgenerator/get_alignment.go
@@ -13,14 +13,13 @@ func (npc *NpcData) setAlignment() {
 	if alignment == api.Alignment_ALIGN_UNKNOWN {
 		if class == api.ClassName_CLASSNAME_PALADIN {
 			chance := r.Intn(99)
-			if chance == 0 {
-				alignment = alignmentList[r.Intn(len(alignmentList))]
+			if chance < 10 {
+				selectWeightedAlignment()
 			} else {
-				nonEvilList := alignmentList[3:]
-				alignment = nonEvilList[r.Intn(len(nonEvilList))]
+				alignment = api.Alignment_ALIGN_LG
 			}
 		} else {
-			alignment = alignmentList[r.Intn(len(alignmentList))]
+			alignment = selectWeightedAlignment()
 		}
 	}
 
@@ -28,16 +27,29 @@ func (npc *NpcData) setAlignment() {
 	npc.npcClass = []*npcClass{&npcClass{className: class}}
 }
 
-var alignmentList = []api.Alignment{
-	api.Alignment_ALIGN_CE,
-	api.Alignment_ALIGN_NE,
-	api.Alignment_ALIGN_LE,
-	api.Alignment_ALIGN_CG,
-	api.Alignment_ALIGN_NG,
-	api.Alignment_ALIGN_LG,
-	api.Alignment_ALIGN_CN,
-	api.Alignment_ALIGN_NN,
-	api.Alignment_ALIGN_LN,
+func selectWeightedAlignment() api.Alignment {
+	weightedSelector := r.Intn(99)
+
+	for alignment, intRange := range weightedAlignments {
+		min := intRange[0]
+		max := intRange[len(intRange)-1]
+		if (weightedSelector > min && weightedSelector < max) || weightedSelector == min || weightedSelector == max {
+			return alignment
+		}
+	}
+	return api.Alignment_ALIGN_NN
+}
+
+var weightedAlignments = map[api.Alignment][]int{
+	api.Alignment_ALIGN_CE: generateIntRange(94, 99),
+	api.Alignment_ALIGN_NE: generateIntRange(88, 93),
+	api.Alignment_ALIGN_LE: generateIntRange(82, 87),
+	api.Alignment_ALIGN_CG: generateIntRange(76, 81),
+	api.Alignment_ALIGN_NG: generateIntRange(70, 75),
+	api.Alignment_ALIGN_LG: generateIntRange(64, 69),
+	api.Alignment_ALIGN_CN: generateIntRange(43, 63),
+	api.Alignment_ALIGN_NN: generateIntRange(0, 21),
+	api.Alignment_ALIGN_LN: generateIntRange(22, 42),
 }
 
 var classList = []api.ClassName{
